6101ffaeb799715fb254f419dddc1961507648c4
itu1982 <34033658+itu1982@users.noreply.github.com>
2022-04-02 19:05:42 +0200

    Add files via upload

---
+++ itu_log/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Itu Log Utilities',
+    "summary": "Permite escribir en los log.",
+    "version": "14.0.0.0.1",
+    "category": "Extra Tools",
+    "website": "https://github.com/itu1982/itu_odoo_addons",
+    "author": "Gorka Iturralde",
+    "license": "AGPL-3",
+    "depends": [],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
