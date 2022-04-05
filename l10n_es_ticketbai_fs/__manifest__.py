f059f9e4bf3707786d7d00526f798bb16cc03326
itu1982 <34033658+itu1982@users.noreply.github.com>
2022-04-05 14:31:48 +0200

    Update __manifest__.py

--- l10n_es_ticketbai_fs/__manifest__.py
+++ l10n_es_ticketbai_fs/__manifest__.py
@@ -2,7 +2,7 @@
 {
     'name' : 'TicketBAI FS',
     "summary": "Agrega la posibilidad de crear facturas simplificadas desde facturacion/contabilidad.",
-    "version": "14.0.0.0.2",
+    "version": "14.0.0.0.4",
     "category": "Accounting & Finance",
     "website": "",
     "author": "Gorka Iturralde",
