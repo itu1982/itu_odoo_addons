af42537973362c97d727aa08d3a94cd295b05811
itu1982 <34033658+itu1982@users.noreply.github.com>
2022-06-04 23:32:15 +0200

    Add files via upload

---
+++ itu_l10n_es_ticketbai_fs/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Itu TicketBAI FS',
+    "summary": "Agrega la posibilidad de crear facturas simplificadas desde facturacion/contabilidad.",
+    "version": "14.0.0.0.6",
+    "category": "Accounting & Finance",
+    "website": "",
+    "author": "Gorka Iturralde",
+    "license": "AGPL-3",
+    "depends": [
+        'account',
+        'point_of_sale',
+        'l10n_es_ticketbai',
+        'l10n_es_ticketbai_api',
+        'itu_log',
+        'itu_original_invoice_data',
+    ],
+    'data': [
+#        'views/template.xml',
+        'views/res_company.xml',
+        'views/account_move.xml',
+        'security/ir.model.access.csv',
+        'data/data_tbai_fs.xml',
+        'data/paper_format.xml',
+        'report/ReceiptOrder2.xml',
+        'report/ReceiptOrder.xml',
+        'report/report_invoice_document.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ itu_l10n_es_ticketbai_fs/models/__init__.py
@@ -0,0 +1,5 @@
+from . import account_move
+from . import tbai_invoice_customer
+from . import tbai_invoice
+from . import res_company
+from . import account_journal
